dddd595197f8873c12f169b2e2101ff08f88dfda
claudio <claudio.sousa@epfl.ch>
2017-07-17 13:51:31 +0200

    lighting leds

---
+++ firmware/src/main.cpp
@@ -0,0 +1,109 @@
+#include <Arduino.h>
+#include <NeoPixelBus.h>
+
+const uint16_t PixelCount = 1;
+const uint8_t PixelPin = 2;
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
+
+#define colorSaturation 128
+
+
+// three element pixels, in different order and speeds
+//NeoPixelBus<NeoRgbFeature, Neo400KbpsMethod> strip(PixelCount, PixelPin);
+
+// For Esp8266, the Pin is omitted and it uses GPIO3 due to DMA hardware use.
+// There are other Esp8266 alternative methods that provide more pin options, but also have
+// other side effects.
+//NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount);
+//
+// NeoEsp8266Uart800KbpsMethod uses GPI02 instead
+
+// You can also use one of these for Esp8266,
+// each having their own restrictions
+//
+// These two are the same as above as the DMA method is the default
+// NOTE: These will ignore the PIN and use GPI03 pin
+//NeoPixelBus<NeoGrbFeature, NeoEsp8266Dma800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, NeoEsp8266Dma400KbpsMethod> strip(PixelCount, PixelPin);
+
+// Uart method is good for the Esp-01 or other pin restricted modules
+// NOTE: These will ignore the PIN and use GPI02 pin
+//NeoPixelBus<NeoGrbFeature, NeoEsp8266Uart800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, NeoEsp8266Uart400KbpsMethod> strip(PixelCount, PixelPin);
+
+// The bitbang method is really only good if you are not using WiFi features of the ESP
+// It works with all but pin 16
+//NeoPixelBus<NeoGrbFeature, NeoEsp8266BitBang800KbpsMethod> strip(PixelCount, PixelPin);
+//NeoPixelBus<NeoRgbFeature, NeoEsp8266BitBang400KbpsMethod> strip(PixelCount, PixelPin);
+
+// four element pixels, RGBW
+//NeoPixelBus<NeoRgbwFeature, Neo800KbpsMethod> strip(PixelCount, PixelPin);
+
+RgbColor red(colorSaturation, 0, 0);
+RgbColor green(0, colorSaturation, 0);
+RgbColor blue(0, 0, colorSaturation);
+RgbColor white(colorSaturation);
+RgbColor black(0);
+
+HslColor hslRed(red);
+HslColor hslGreen(green);
+HslColor hslBlue(blue);
+HslColor hslWhite(white);
+HslColor hslBlack(black);
+
+
+void setup()
+{
+    Serial.begin(115200);
+    while (!Serial); // wait for serial attach
+
+    Serial.println();
+    Serial.println("Initializing...");
+    Serial.flush();
+
+    // this resets all the neopixels to an off state
+    strip.Begin();
+    strip.Show();
+
+
+    Serial.println();
+    Serial.println("Running...");
+}
+
+
+void loop()
+{
+
+    Serial.println("Colors R, G, B, W...");
+
+    // set the colors,
+    // if they don't match in order, you need to use NeoGrbFeature feature
+     strip.SetPixelColor(0, red);
+    // strip.SetPixelColor(1, green);
+    // strip.SetPixelColor(2, blue);
+    // strip.SetPixelColor(3, white);
+    // the following line demonstrates rgbw color support
+    // if the NeoPixels are rgbw types the following line will compile
+    // if the NeoPixels are anything else, the following line will give an error
+    //strip.SetPixelColor(3, RgbwColor(colorSaturation));
+    strip.Show();
+
+
+    delay(1000);
+
+    Serial.println("Off ...");
+
+    // turn off the pixels
+     strip.SetPixelColor(0, black);
+    // strip.SetPixelColor(1, black);
+    // strip.SetPixelColor(2, black);
+    // strip.SetPixelColor(3, black);
+    strip.Show();
+
+    delay(1000);
+
+
+    strip.Show();
+
+}
+
